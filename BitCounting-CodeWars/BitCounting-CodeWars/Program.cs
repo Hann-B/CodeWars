dd7cc5a38602fbde9e82a01652882797cdb78f7d
hanna <hlbernard124@gmail.com>
2017-06-05 11:27:28 -0400

    The Coupon Code

--- BitCounting-CodeWars/BitCounting-CodeWars/Program.cs
+++ BitCounting-CodeWars/BitCounting-CodeWars/Program.cs
@@ -8,6 +8,14 @@ namespace BitCounting_CodeWars
 {
     class Program
     {
+        //input = number
+        public static int CountBits(int n)
+        {
+            //number to binary
+            var binary = Convert.ToString(n, 2);
+            return binary.Count(ch => ch == '1');
+        }
+
         static void Main(string[] args)
         {
         }
---
+++ TheCouponCode-Codewars/TheCouponCode-Codewars/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TheCouponCode_Codewars
+{
+    class Program
+    {
+        public static class Kata
+        {
+            public static bool CheckCoupon(string enteredCode, string correctCode, string currentDate, string expirationDate)
+            {
+                var isValid = false;
+                if(enteredCode==correctCode && DateTime.Parse(currentDate)<=DateTime.Parse(expirationDate))
+                {
+                    return isValid=true;
+                }
+                return isValid;
+            }
+        }
+
+
+        static void Main(string[] args){}
+    }
+}
