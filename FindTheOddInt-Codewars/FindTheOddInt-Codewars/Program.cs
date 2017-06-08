a47375153ac930b4b17d91cc18e90a594a44881b
hanna <hlbernard124@gmail.com>
2017-06-07 20:39:15 -0400

    Find the Odd Int

---
+++ FindTheOddInt-Codewars/FindTheOddInt-Codewars/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace FindTheOddInt_Codewars
+{
+    class Program
+    {
+        
+        public static int find_it(int[] seq)
+        {
+            int count=0;
+            var groups = seq.GroupBy(n => n);
+            foreach (var group in groups)
+            {
+                if (group.Count() % 2 != 0)
+                {
+                    count = group.Key;
+                }               
+            }
+            return count;
+                
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
