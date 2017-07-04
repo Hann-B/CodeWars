2710eb49ebb59e7ffb67227b78d0d05231d0ba3c
hanna <hlbernard124@gmail.com>
2017-07-04 14:15:09 -0400

    PHP

---
+++ Duck Duck Goose - Codewars/Duck Duck Goose - Codewars/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Duck_Duck_Goose___Codewars
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ FindtheSmallestIntegerInTheArray-PHP-Codewars/SmallestInteger.php
@@ -0,0 +1,7 @@
+<?php
+$arr=array[3,5,10,1,4,55];
+
+function smallestInteger ($arr){
+    return min($arr);
+}
+?>
