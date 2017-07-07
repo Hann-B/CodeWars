f29793338f6d6e7b28e27966b3d4492a58e77687
hanna <hlbernard124@gmail.com>
2017-07-07 15:49:38 -0400

    Credit Card Mask C#

---
+++ CreditCardMask-Codewars/CreditCardMask-Codewars/Mask.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CreditCardMask
+{
+    public class Mask
+    {
+        public static string Maskify(string cc)
+        {
+            var rv = string.Empty;
+            if (cc.Length > 4)
+            {
+                for (int i = 0; i < cc.Length - 4; i++)
+                {
+                    rv += "#";
+                }
+                rv += cc.Substring(cc.Length - 4);
+            }
+            else
+            {
+                rv = cc;
+            }
+            return rv;
+        }
+    }
+}
