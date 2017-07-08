953457febd802c37a958fa1bf75496a95ce47037
hanna <hlbernard124@gmail.com>
2017-07-08 19:52:09 -0400

    Do I get a bonus? C#

---
+++ DoIGetABonus-Codewars/DoIGetABonus-Codewars/Bonus.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DoIGetABonus
+{
+    public class Bonus
+    {
+        public static string bonus_time(int salary, bool bonus)
+        {
+            var total = salary;
+            if (bonus == true)
+            {
+                total = salary * 10;
+            }
+            return $"${total}";
+        }
+    }
+}
+
