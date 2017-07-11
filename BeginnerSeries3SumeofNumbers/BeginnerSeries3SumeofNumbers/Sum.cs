960a4f9501d4306ea75f83b72a088ecc1af48833
hanna <hlbernard124@gmail.com>
2017-07-11 14:26:14 -0400

    Beginner Series #3 Sum of Numbers in C#

---
+++ BeginnerSeries3SumeofNumbers/BeginnerSeries3SumeofNumbers/Sum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeginnerSeries3SumeofNumbers
+{
+    public class Sum
+    {
+        public int GetSum(int a, int b)
+        {
+            if (a == b)
+            {
+                return a;
+            }
+            else
+            {
+                var min = Math.Min(a, b);
+                var max = Math.Max(a, b);
+                
+                return (max + min)*(max - min + 1) / 2;
+            }
+            
+        }
+    }
+}
