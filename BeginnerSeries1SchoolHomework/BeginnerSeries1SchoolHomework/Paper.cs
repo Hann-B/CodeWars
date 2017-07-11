99f5cb578be2a879cd109a594dc3261f0caa72b0
hanna <hlbernard124@gmail.com>
2017-07-11 09:54:45 -0400

    Beginner Series #1 School Paperwork

---
+++ BeginnerSeries1SchoolHomework/BeginnerSeries1SchoolHomework/Paper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeginnerSeries1SchoolHomework
+{
+    public class Paper
+    {
+        public int Paperwork(int n, int m)
+        {
+            if (n < 0 || m < 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return n * m;
+            }
+            
+        }
+    }
+}
