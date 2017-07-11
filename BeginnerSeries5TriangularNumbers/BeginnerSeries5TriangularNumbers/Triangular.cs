8d5b26c7a6f4b5b4b31787eea05e404a2536445d
hanna <hlbernard124@gmail.com>
2017-07-11 14:40:49 -0400

    BeginnerSeries #5 Triangular Numbers C#

---
+++ BeginnerSeries5TriangularNumbers/BeginnerSeries5TriangularNumbers/Triangular.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeginnerSeries5TriangularNumbers
+{
+    public class Triangular
+    {
+        public bool isTriangular(int T)
+        {
+            return true ? (Math.Floor(Math.Sqrt(8 * T + 1)) == Math.Sqrt(8 * T + 1)) : false;
+        }
+    }
+}
