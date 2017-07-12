292f2b9c886ff0a0e12ad487d77cde400aae8a09
hanna <hlbernard124@gmail.com>
2017-07-12 15:54:26 -0400

    Multiples of 3 and 5 C#

---
+++ MultiplesOf3And5/MultiplesOf3And5/Kata.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MultiplesOf3And5
+{
+    public class Kata
+    {
+        public static int Solution(int value)
+        {
+            var rv = 0;
+            List<int> multiples = new List<int>();
+            for (int i = 1; i < value; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0) 
+                {
+                    multiples.Add(i);
+                }               
+            }
+            rv = multiples.Sum();
+            return rv;
+        }
+    }
+}
