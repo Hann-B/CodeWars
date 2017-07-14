7684f8c021a25c118ee4b489735ec4844611a239
hanna <hlbernard124@gmail.com>
2017-07-14 10:50:36 -0400

    Is this a triangle C#

---
+++ IsThisATriangle/IsThisATriangle/Triangle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IsThisATriangle
+{
+    public class Triangle
+    {
+        public static bool IsTriangle(int a, int b, int c)
+        {           
+            return true?(a + b > c && a + c > b && b + c > a):false;
+        }
+    }
+}
