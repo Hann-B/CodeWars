325cc75b377026187846005f70ce0c5d5b6508eb
hanna <hlbernard124@gmail.com>
2017-07-14 10:08:08 -0400

    Turn any word into a beef taco c#

---
+++ TurnAnyWordIntoABeefTaco/TurnAnyWordIntoABeefTaco/Kata.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TurnAnyWordIntoABeefTaco
+{
+    public class Kata
+    {
+        public static string[] Tacofy(string word)
+        {
+            var list = new List<string>();
+            list.Add("shell");
+            var letters = word.ToLower().ToCharArray();
+            foreach (var letter in letters)
+            {
+                switch (letter)
+                {
+                    case 'a':
+                    case 'e':
+                    case 'i':
+                    case 'o':
+                    case 'u':
+                        list.Add("beef");
+                        break;
+                    case 't':
+                        list.Add("tomato");
+                        break;
+                    case 'l':
+                        list.Add("lettuce");
+                        break;
+                    case 'c':
+                        list.Add("cheese");
+                        break;
+                    case 'g':
+                        list.Add("guacamole");
+                        break;
+                    case 's':
+                        list.Add("salsa");
+                        break;
+                }
+            }
+            list.Add("shell");
+            var rv = list.ToArray();
+            return rv;
+        }
+    }
+}
