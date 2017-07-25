80c4a8c155fff322185a4329a42277834c93708f
hanna <hlbernard124@gmail.com>
2017-07-25 19:53:44 -0400

    Lost number in number sequence C#

---
+++ LostNumberInNumberSequence/LostNumberInNumberSequence/Kata.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LostNumberInNumberSequence
+{
+    public class Kata
+    {
+        public static int FindDeletedNumber(List<int> startingList, List<int> mixedList)
+        {           
+            return startingList.Sum() - mixedList.Sum();
+        }
+    }
+}
