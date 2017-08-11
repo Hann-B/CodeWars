1b696d2a7a5c36242c46d2811845c3ec818781a8
hanna <hlbernard124@gmail.com>
2017-08-10 20:29:31 -0800

    Reverse a Number c#

---
+++ ReverseANumber/ReverseANumber/Kata.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReverseANumber
+{
+    public class Kata
+    {
+        public int ReverseNumber(int n)
+        {
+            var reversed = 0;
+            while (n != 0)
+            {
+                reversed= reversed * 10 + n % 10;
+                n = n / 10;
+            }
+
+            return reversed;
+        }
+    }
+}
