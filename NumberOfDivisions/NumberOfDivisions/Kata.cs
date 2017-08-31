d7320ef8571fa175b84d60831910b6988a76d769
hanna <hlbernard124@gmail.com>
2017-08-31 16:43:29 -0400

    number of divions c#

---
+++ NumberOfDivisions/NumberOfDivisions/Kata.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NumberOfDivisions
+{
+    public class Kata
+    {
+        public static int Divisions(int n, int divisor)
+        {
+            int count = 0;
+            while (n / divisor > 0)
+            {
+                n /= divisor;
+                count = count + 1;
+
+            }
+            return count;
+        }
+    }
+}
