765af880b8c8f363184eb9307a8438953aff56e9
hanna <hlbernard124@gmail.com>
2017-10-10 18:38:10 -0400

    A Strange Trip To The Market, C#

---
+++ AStrangeTripToTheMarket/AStrangeTripToTheMarket/Kata.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AStrangeTripToTheMarket
+{
+    public static class Kata
+    {
+        public static bool IsLockNessMonster(string sentence)
+        {
+            var rv = true;
+            if (sentence.ToLower().Contains("tree fiddy")||
+                sentence.ToLower().Contains("3.50") ||
+                sentence.ToLower().Contains("three fifty"))
+            {
+                rv = true;
+            }
+            else
+            {
+                rv = false;
+            }
+            return rv;
+        }
+    }
+}
